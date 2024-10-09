529251b60c703eb1ebcd6ce6ae2a87a2876d2237
nagar <nagar@DESKTOP-52BFLJ9>
2024-10-09 16:39:34 +0530

    hello

---
+++ com/javaprograms/p1/TestCode.java
@@ -0,0 +1,10 @@
+package com.javaprograms.p1;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ com/javaprograms/p1/package-info.java
@@ -0,0 +1,1 @@
+package com.javaprograms.p1;
